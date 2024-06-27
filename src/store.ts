36f3a9c12c8a60bf8fe10aa531bb2b22805a53a1
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-06-27 21:07:59 +0800

    add rxjs behaviorSubject

---
+++ src/store.ts
@@ -0,0 +1,38 @@
+//what is it ?
+//behaviroSubject store most recent value and made it easy to use
+import { BehaviorSubject, map } from 'rxjs';
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+  power?: number;
+}
+
+export const rawPokeMon$ = new BehaviorSubject<Pokemon[]>([]);
+
+//Map came from rxjs
+export const pokenMonWithPower$ = rawPokeMon$.pipe(
+  map((pokenMon) =>
+    pokenMon.map((p) => ({
+      ...p,
+      power:
+        p.hp +
+        p.attack +
+        p.defense +
+        p.special_attack +
+        p.special_defense +
+        p.speed,
+    }))
+  )
+);
+
+fetch('./pokemon-simplified.json')
+  .then((res) => res.json())
+  .then((data) => rawPokeMon$.next(data));
